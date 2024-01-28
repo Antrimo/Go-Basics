3319199d4c5c5f4a77e44989b25572a826dc13f0
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-01-28 23:12:21 +0530

    var const and fomating

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Golang Users!")
+	var name = "Go Conference"  //variable declaration
+	const conferenceTicket = 50 //constant declaration
+	var remainingTicket = 50
+	fmt.Printf("Welcome to %v booking application \n", conferenceTicket)
+	fmt.Println("Total ticket available for", name, "is", conferenceTicket, "and remaining ticket is", remainingTicket)
+	fmt.Println("Get your ticket now!!!")
+
 }
