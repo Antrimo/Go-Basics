aa063caa992d074bc6dae8e2fcaf9f5b2392e359
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-01-30 19:50:53 +0530

    data types and formatting

--- main.go
+++ main.go
@@ -6,8 +6,18 @@ func main() {
 	var name = "Go Conference"  //variable declaration
 	const conferenceTicket = 50 //constant declaration
 	var remainingTicket = 50
+
+	fmt.Printf("name is %T, conferenceTicket is %T, remainingTicket is %T \n", name, conferenceTicket, remainingTicket)
+
 	fmt.Printf("Welcome to %v booking application \n", conferenceTicket)
 	fmt.Println("Total ticket available for", name, "is", conferenceTicket, "and remaining ticket is", remainingTicket)
 	fmt.Println("Get your ticket now!!!")
+
+	var userName string //ask user for their name
+	var userTicket int  //ask user for number of ticket they want to buy
+
+	userName = "John Doe" //assign user name to variable
+	userTicket = 5        //assign user ticket to variable
+	fmt.Printf("User %v booked %v ticket", userName, userTicket)
 
 }
